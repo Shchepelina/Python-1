3a97eb700948073015172d4e20ab6b84f58c559c
shchepelina <apt_dobdoctora@mail.ru>
2022-12-25 22:39:13 +0300

    2

--- HomeWork/HM1/2.py
+++ HomeWork/HM1/2.py
@@ -1,9 +1,9 @@
 #2. Напишите программу для проверки ложности утверждения
 #(W ⋀ Z) ⋁ ¬Y ⋁ (¬X ≡ ¬W) для всех значений предикат.
 
-for x in range(2):
-        for y in range(2):
-            for z in range(2):
-                print(not (x or y or z) == (not x and not y and not z))
-                print(x, y, z)
+for W in range(2):
+        for Z in range(2):
+            for V in range(2):
+                print(not (W or Z or V) == (not W and not Z and not V))
+                print(W, V, Z)
 
